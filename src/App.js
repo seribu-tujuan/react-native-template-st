94011c722c2315fb9a1eb255bf5747e315a949bd
Nico Dinata <ncdnt@icloud.com>
2019-08-19 19:49:01 +1000

    Add Flow to list of dependencies

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@
  * @flow
  */
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import React, { Component } from 'react';
+import { Platform, StyleSheet, Text, View } from 'react-native';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -22,7 +22,7 @@ export default class App extends Component<Props> {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
+        <Text style={styles.instructions}>Seribu Tujuan Template</Text>
         <Text style={styles.instructions}>{instructions}</Text>
       </View>
     );
