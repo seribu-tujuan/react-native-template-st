e2ba517a5f6cdd53ad379f49978dc0634a3f758f
Nico Dinata <ncdnt@icloud.com>
2019-08-20 22:06:16 +1000

    Copy MainActivity.java with React Navigation configuration

--- cleanup.js
+++ cleanup.js
@@ -1,20 +1,45 @@
 const fs = require('fs');
 const path = require('path');
 
-const projectFilesToDelete = ['App.js']
-const templateFilesToDelete = ['cleanup.js']
+const projectPath = path.join(__dirname, '..', '..');
+const projectFile = require(path.join(projectPath, 'app.json'));
+const projectName = projectFile.name;
 
-const deleteFile = filePath => {
-  if (!fs.existsSync(filePath)) {
-    return
-  }
+// copy MainActivity.java to complete React Navigation configuration
+const mainActivitySrc = path.join(__dirname, 'MainActivity.java');
+const mainActivityDest = path.join(
+  projectPath,
+  `android/app/src/main/java/com/${projectName}/MainActivity.java`,
+);
 
-  fs.unlinkSync(filePath)
+if (fs.existsSync(path.join(projectPath, 'android/'))) {
+  fs.copyFileSync(mainActivitySrc, mainActivityDest);
 }
 
-const projectPath = path.join(__dirname, '..', '..')
-const deleteProjectFile = fileName => deleteFile(path.join(projectPath, fileName))
-const deleteTemplateFile = fileName => deleteFile(path.join(__dirname, fileName))
+// delete unnecessary project and template files
+const projectFilesToDelete = ['App.js'];
+const templateFilesToDelete = [
+  'cleanup.js',
+  'LICENSE',
+  'README.md',
+  'MainActivity.java',
+];
 
-projectFilesToDelete.forEach(deleteProjectFile)
-templateFilesToDelete.forEach(deleteTemplateFile)
+const deleteFile = (filePath) => {
+  if (!fs.existsSync(filePath)) {
+    return;
+  }
+
+  fs.unlinkSync(filePath);
+};
+
+const deleteProjectFile = (fileName) => {
+  deleteFile(path.join(projectPath, fileName));
+};
+
+const deleteTemplateFile = (fileName) => {
+  deleteFile(path.join(__dirname, fileName));
+};
+
+projectFilesToDelete.forEach(deleteProjectFile);
+templateFilesToDelete.forEach(deleteTemplateFile);
