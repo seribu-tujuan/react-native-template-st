f53088e7e9cfc8e751e198a3f6e15ddc2211e877
Nico Dinata <ncdnt@icloud.com>
2019-08-22 19:36:20 +1000

    Separate cleanup script from navigation setup one

--- cleanup.js
+++ cleanup.js
@@ -2,19 +2,6 @@ const fs = require('fs');
 const path = require('path');
 
 const projectPath = path.join(__dirname, '..', '..');
-const projectFile = require(path.join(projectPath, 'app.json'));
-const projectName = projectFile.name;
-
-// copy MainActivity.java to complete React Navigation configuration
-const mainActivitySrc = path.join(__dirname, 'MainActivity.java');
-const mainActivityDest = path.join(
-  projectPath,
-  `android/app/src/main/java/com/${projectName}/MainActivity.java`,
-);
-
-if (fs.existsSync(path.join(projectPath, 'android/'))) {
-  fs.copyFileSync(mainActivitySrc, mainActivityDest);
-}
 
 // delete unnecessary project and template files
 const projectFilesToDelete = ['App.js'];
@@ -23,6 +10,7 @@ const templateFilesToDelete = [
   'LICENSE',
   'README.md',
   'MainActivity.java',
+  'setupNavigation.js',
 ];
 
 const deleteFile = (filePath) => {
