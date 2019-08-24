e1a7d33cf0b7c844028bc4fe6760a0f296e35125
Nico Dinata <ncdnt@icloud.com>
2019-08-25 00:45:24 +1000

    Add script for ESLint CLI

--- setupConfig.js
+++ setupConfig.js
@@ -8,5 +8,10 @@ const packageJsonFile = JSON.parse(fs.readFileSync(packageJsonPath), 'utf8');
 // add Prettier script to package.json
 const prettierScript = "prettier --config .prettierrc --write \"src/**/*.js\"";
 packageJsonFile.scripts.pretty = prettierScript;
+
+// add ESLint script to package.json
+const eslintScript = "eslint \"src/**/*.js\"";
+packageJsonFile.scripts.lint = eslintScript;
+
 const packageJsonString = JSON.stringify(packageJsonFile, null, 2);
 fs.writeFileSync(packageJsonPath, packageJsonString);
