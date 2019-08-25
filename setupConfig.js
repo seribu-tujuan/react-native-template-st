72f8e7e6db88b413a6d74abf3be4789b418b9b1e
Nico Dinata <ncdnt@icloud.com>
2019-08-25 21:13:43 +1000

    Update extension of config files

--- setupConfig.js
+++ setupConfig.js
@@ -6,11 +6,11 @@ const packageJsonPath = path.join(projectPath, 'package.json');
 const packageJsonFile = JSON.parse(fs.readFileSync(packageJsonPath), 'utf8');
 
 // add Prettier script to package.json
-const prettierScript = "prettier --config .prettierrc --write \"src/**/*.js\"";
+const prettierScript = 'prettier --config .prettierrc.json --write "src/**/*.js"';
 packageJsonFile.scripts.pretty = prettierScript;
 
 // add ESLint script to package.json
-const eslintScript = "eslint \"src/**/*.js\"";
+const eslintScript = 'eslint "src/**/*.js"';
 packageJsonFile.scripts.lint = eslintScript;
 
 const packageJsonString = JSON.stringify(packageJsonFile, null, 2);
