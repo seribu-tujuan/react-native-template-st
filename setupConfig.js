aecf889afe7539bf1dca4579a4e203abfe51b1f0
Nico Dinata <ncdnt@icloud.com>
2019-08-26 21:34:40 +1000

    Add pre-commit hooks with `husky` and `lint-staged`

--- setupConfig.js
+++ setupConfig.js
@@ -6,12 +6,27 @@ const packageJsonPath = path.join(projectPath, 'package.json');
 const packageJsonFile = JSON.parse(fs.readFileSync(packageJsonPath), 'utf8');
 
 // add Prettier script to package.json
-const prettierScript = 'prettier --config .prettierrc.json --write "src/**/*.js"';
-packageJsonFile.scripts.pretty = prettierScript;
+const prettierScript =
+  'prettier --config .prettierrc.json --write "src/**/*.js"';
+packageJsonFile.scripts['pretty'] = prettierScript;
 
 // add ESLint script to package.json
 const eslintScript = 'eslint "src/**/*.js"';
-packageJsonFile.scripts.lint = eslintScript;
+packageJsonFile.scripts['lint'] = eslintScript;
+
+// set up `husky` and `lint-staged`
+packageJsonFile['husky'] = {
+  hooks: {
+    'pre-commit': 'lint-staged',
+  },
+};
+packageJsonFile['lint-staged'] = {
+  "src/**/*.js": [
+    'prettier --config .prettierrc.json --write',
+    'eslint',
+    'git add',
+  ]
+}
 
 const packageJsonString = JSON.stringify(packageJsonFile, null, 2);
 fs.writeFileSync(packageJsonPath, packageJsonString);
